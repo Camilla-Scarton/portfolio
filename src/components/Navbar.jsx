d2a50fb9969cf693088bd9325bc9ddad1ea78035
Camilla Scarton <camy.s96@gmail.com>
2023-05-02 15:37:37 +0200

    scroll animated div

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Camilla &nbsp;
-            <span className="sm:block hidden">| Scarton</span>
+            <span className="sm:block hidden">Scarton</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
